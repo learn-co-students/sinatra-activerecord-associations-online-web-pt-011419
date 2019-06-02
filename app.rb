aac89266c1388ca7698b83987934f463a219bb85
Andrea Lovewell <andy.lovewell@gmail.com>
2019-06-02 05:11:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+Cat.create(:name => "Maru", :age => 3, :breed => "Scottish Fold")
+Cat.create(:name => "Hannah", :age => 2, :breed => "Tabby")
+Cat.create(:name => "Patches", :age => 2, :breed => "Calico")
+
+Owner.create(:name => "Sophie")
+Owner.create(:name => "Ann")
 end
